3b1da4a412cb73fe0c10e87c2ade2dc1bfcccbd4
gkaguirre <gkaguirre@me.com>
2024-03-13 10:51:29 -0400

    Generating covariates from pulse ox recordings

--- code/fMRI/analysis/analyseCombiPuffData/returnPhysioMatrix.m
+++ code/fMRI/analysis/analyseCombiPuffData/returnPhysioMatrix.m
@@ -1,9 +1,9 @@
-function physioMatrix = returnPhysioMatrix(fwSessID)
+function [physioMatrix,fileNameStem] = returnPhysioMatrix(fwSessID,tr)
 
 %{
-fwSessID = '6553f6199caa786dfb1b24b6';
-physioMatrix = returnPhysioMatrix(fwSessID)
-
+    fwSessID = '65f083a829fdb99849fa2e95';
+    tr = 2.040;
+    [physioMatrix,fileNameStem] = returnPhysioMatrix(fwSessID,tr);
 %}
 
 
@@ -11,16 +11,59 @@ physioMatrix = returnPhysioMatrix(fwSessID)
 fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
 acquisitionList = fw.getSessionAcquisitions(fwSessID);
 
-% Download the acquisitions; skip over the Phoenix file
-acqIdx = [7:21,23:25];
-nAcq = length(acqIdx);
-for ii = 1:nAcq
-    acquisitionId{ii} = acquisitionList{acqIdx(ii)}.id;
-    fileID = acquisitionList{acqIdx(ii)}.files{3}.fileId;
-    fileName{ii} = acquisitionList{acqIdx(ii)}.files{3}.name;
-    savePath = fullfile(saveDir,fileName{ii});
-    % fw.downloadFileFromAcquisition(acquisitionId{ii},fileName{ii},savePath);
-end
+% Identify a temp directory
+workDir = tempdir();
 
+% Step through the acquisitions, find the physio files, download the physio
+% dicom
+fileNameStem = [];
+nPhysioFiles = 0;
+for ii = 1:length(acquisitionList)
+
+    % Check the acquisition label for the physio suffix
+    if contains(acquisitionList{ii}.label,'_PhysioLog')
+
+        % Increment the total number of physio files we have found
+        nPhysioFiles = nPhysioFiles+1;
+
+        % Download the physio DICOM from Flywheel
+        fileName = acquisitionList{ii}.files{1}.name;
+        savePath = fullfile(workDir,fileName);
+        fw.downloadFileFromAcquisition(acquisitionList{ii}.id,fileName,savePath);
+
+        % Convert the DICOM to ".log" physio files
+        dicomInfo = readCMRRPhysio(savePath, 0, workDir);
+
+        % Extract some information about the acquisition to be used later
+        % for the tapas analysis
+        nScans = size(dicomInfo.SliceMap,2);
+        nSlices = size(dicomInfo.SliceMap,3);
+
+        % Rename the PULS and Info files to have the same prefix as the
+        % acquisition name
+        tmp = dir(fullfile(workDir,['*' dicomInfo.UUID{1} '_Info.log']));
+        fileIn = fullfile(tmp.folder,tmp.name);
+        fileOutInfo = fullfile(workDir,strrep(fileName,'_PhysioLog.dcm','_PhysioLog_Info.log'));
+        movefile(fileIn,fileOutInfo);
+        tmp = dir(fullfile(workDir,['*' dicomInfo.UUID{1} '_PULS.log']));
+        fileIn = fullfile(tmp.folder,tmp.name);
+        fileOutPULS = fullfile(workDir,strrep(fileName,'_PhysioLog.dcm','_PhysioLog_PULS.log'));
+        movefile(fileIn,fileOutPULS);
+
+        % Store the file path and stem name of these physio log files
+        fileNameStem{nPhysioFiles} = strrep(savePath,'.dcm','');
+
+        % Create a "tapasStruct" that has the information needed for the
+        % tapas routine
+        saveDir = fullfile(workDir,fileNameStem{nPhysioFiles});
+        tapasStruct = createTapasStruct(saveDir,fileOutPULS,fileOutInfo,tr,nScans,nSlices);
+
+        % Call the tapas analysis
+        physioStruct = tapas_physio_main_create_regressors(tapasStruct);
+
+        % Save the returned covariates into a cell array
+        physioMatrix{nPhysioFiles} = physioStruct.model.R;
+    end
+end
 
 end
