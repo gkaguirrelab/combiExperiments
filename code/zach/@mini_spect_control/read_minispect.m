56a871cd080ac7e14121532948d131027fec7a97
Zachary Kelly <zacharykelly@brb-04063.apn.wlan.private.upenn.edu>
2024-06-20 10:15:41 -0400

    move my code to repo

---
+++ code/zach/@mini_spect_control/read_minispect.m
@@ -0,0 +1,13 @@
+function lines = read_minispect(obj)
+    lines = "";
+   
+    writeline(obj.serialObj,'R');     
+
+    for i = 1:15
+      lines = lines + readline(obj.serialObj) + newline;
+    end 
+    
+    disp(lines);
+    
+
+    
