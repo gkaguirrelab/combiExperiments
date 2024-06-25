4e24db597c7206544c04bba0d729c14a506d64cf
Zachary Kelly <zjkelly19@gmail.com>
2024-06-25 10:32:18 -0400

    fix channel parsing error

--- code/minispect/@mini_spect_control/parse_channel_reading.m
+++ code/minispect/@mini_spect_control/parse_channel_reading.m
@@ -1,18 +1,28 @@
 function channel_values = parse_channel_reading(obj, reading)
-    channel_values = zeros(1,13);
 
+    disp(reading);
+
+    channel_values = zeros(1,10);
+
+    disp(size(reading,2))
+
+    debugging_output_offset = 0; 
     for i = 1:size(reading,2)
         text_val_separators = strfind(reading{1,i}, ':');
         if(isempty(text_val_separators))
+            debugging_output_offset = debugging_output_offset + 1; 
             continue
         end
         
         text_val_seperator_indx = text_val_separators(1);
 
         reading_as_charvec = char(reading{i});
+        
+        disp(reading_as_charvec)
+
         channel_val = str2num( strtrim( reading_as_charvec(text_val_seperator_indx+1:end) ) );
 
-        channel_values(i) = channel_val;
+        channel_values(i-debugging_output_offset) = channel_val;
     end
 
 end
--- code/minispect/calibrate_minispect.m
+++ code/minispect/calibrate_minispect.m
@@ -1,19 +1,27 @@
-addpath('/Users/zacharykelly/Documents/MATLAB/toolboxes/combiLEDToolbox/code');
-
 % Parameters
 nPrimarySteps = 10; 
 nSamplesPerStep = 10;
 
+calFileName = 'CombiLED_shortLLG_testSphere_ND0x2.mat';
+calDir = '/Users/zacharykelly/Documents/MATLAB/projects/combiExperiments/cal';
+load(fullfile(calDir,calFileName),'cals');
+cal = cals{end};
+
 % Initialize combiLED object
 CL = CombiLEDcontrol();
+
+% Update the gamma table
+CL.setGamma(cal.processedData.gammaTable);
 
 % Initialize minispect object
 MS = mini_spect_control();
 
 % Initialize the chip we want and the mode for it to be in
 chip = MS.chip_name_map("ASM7341");
-mode = MS.chip_functions_map("Channels");
+chip_functions = MS.chip_functions_map(chip);
+mode = chip_functions('Channels');
 
+% Arrays to hold outputs over time series
 combi_intensities = {};
 means = {};
 standard_deviations = {};
@@ -26,19 +34,29 @@ for i = 1:nPrimarySteps
 
     % Set the CombiLED settings
     CL_settings = channel_intensity * ones(1,8);
-    CL.setPrimaries(Cl_settings);
+    CL.setPrimaries(CL_settings);
     
     % Initialize matrix where Row_i = sample_i, col_i = channel_i 
-    channel_readings_matrix = zeros(nSamplesPerStep,13);
+    channel_readings_matrix = zeros(nSamplesPerStep,10);
     
+    % get the mean and std of each col (channel) over this matrix at 
+    % this Primary step. We are going to plot all of these later by primary step 
+
     % Record N samples from the minispect
     for j = 1:nSamplesPerStep
         channel_values = MS.read_minispect(chip,mode); 
 
-        channel_readings_matrix(j) = channel_values; 
+        channel_readings_matrix(j,:) = channel_values; 
     end
 
     disp(channel_readings_matrix)
 
 
 end
+
+% Close the serial ports with the devices
+CL.serialClose();
+MS.serialClose_minispect()
+
+clear CL; 
+clear MS; 
