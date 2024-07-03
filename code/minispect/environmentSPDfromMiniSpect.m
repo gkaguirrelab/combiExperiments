42107ebb2dcc614e95dfa31056caa5176c65010c
gkaguirre <gkaguirre@me.com>
2024-07-03 12:02:06 -0400

    Routine to estimate environmental SPD

--- code/minispect/environmentSPDfromMiniSpect.m
+++ code/minispect/environmentSPDfromMiniSpect.m
@@ -1,8 +1,8 @@
-function [spd,modelWeights] = environmentSPDfromMiniSpect( detectorWeights )
+function [estimatedSPD,sourceWeights,fVal] = environmentSPDfromMiniSpect( detectorWeights )
 % SPD from natural and artificial light sources to fit minispect weights
 %
 % Syntax:
-%   [spd,modelWeights] = environmentSPDfromMiniSpect( detectorWeights )
+%   [estimatedSPD,sourceWeights,fVal] = environmentSPDfromMiniSpect( detectorWeights )
 %
 % Description:
 %   We wish to reconstruct the environmental spectral power distribution
@@ -34,16 +34,19 @@ function [spd,modelWeights] = environmentSPDfromMiniSpect( detectorWeights )
 %
 % Examples:
 %{
-	foo = 1;
-    bar = myFunc(foo);
-	fprintf('Bar = %d \n',bar);   
+	detectorWeights = [307, 466, 1024, 1401, 1464, 1259, 1570, 444, 3740, 228];
+    [estimatedSPD,sourceWeights,fVal] = environmentSPDfromMiniSpect(detectorWeights);
 %}
 
-% Load the minispect SPDs
+% Load the detector SPDs
 miniSpectSPDPath = fullfile(tbLocateProjectSilent('combiExperiments'),'data','ASM7341_spectralSensitivity.mat');
 load(miniSpectSPDPath,'T');
-minispectS = WlsToS(T.wl);
-minispectP = T{:,2:end};
+detectorS = WlsToS(T.wl);
+detectorP = T{:,2:end};
+nChannels = size(detectorP,2);
+
+% Check that the length of the detectorWeights vec matches nChannels
+assert(nChannels == length(detectorWeights));
 
 % Load the set of light sources
 enviroSPDPath = fullfile(tbLocateProjectSilent('combiExperiments'),'data','CIEDaylightComponents.mat');
@@ -52,7 +55,30 @@ enviroS = WlsToS(CIEDaylightComponents_T.wls);
 enviroP = CIEDaylightComponents_T{:,2:end};
 
 % Reformat the minispect SPDs to be in the space of the light sources
-minispectP_resamp = [];
-for ii = 1:nPrimaries
-    minispectP_resamp(:,ii) = interp1(SToWls(sourceS),sourceP(:,ii),SToWls(D65S));
+detectorP_resamp = [];
+for ii = 1:nChannels
+    detectorP_resamp(:,ii) = interp1(SToWls(detectorS),detectorP(:,ii),SToWls(enviroS));
+end
+
+% Create a forward model of the minispect weights based upon a combination
+% of light sources
+mySPD = @(x) enviroP*x';
+myDetectorWeights = @(x) mySPD(x)'*detectorP_resamp;
+myObj = @(x) norm(myDetectorWeights(x)-detectorWeights);
+
+% Set the fmincon options
+options = optimset('fmincon');
+options.Display = 'off';
+
+% Set the bounds. For the daylight components, it is possible to have
+% negative loadings on the 2nd and 3rd component. Otherwise, all components
+% are bound by zero.
+lb = [0 -Inf -Inf];
+ub = [Inf Inf Inf];
+
+% Find the light source parameters that best fit the observed detector
+% weights
+[sourceWeights,fVal] = fmincon(myObj,[1 1 1],[],[],[],[],lb,ub,[],options);
+estimatedSPD = mySPD(sourceWeights);
+
 end
