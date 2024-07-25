8179e32873b20c21b87ed63535e38022cc18dd19
Zachary Kelly <zjkelly19@gmail.com>
2024-07-25 08:59:36 -0400

    try to inspect camera properties

--- code/minispect/raspberry_pi_firmware/utility/Camera_util.py
+++ code/minispect/raspberry_pi_firmware/utility/Camera_util.py
@@ -1,8 +1,9 @@
 from picamera2 import Picamera2
 
 def main():
-    picam2 = Picamera2()
-    picam2.start_and_record_video("Desktop/new_video.mp4", duration=5, show_preview=True)
+    picam2: Picamera2 = Picamera2()
+    print(picam2.camera_controls.items())
+
     picam2.close()
 
 
