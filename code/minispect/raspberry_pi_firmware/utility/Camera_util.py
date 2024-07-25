14ed800b7ea48c07cf4feef082aa70a66de541a7
Zachary Kelly <zjkelly19@gmail.com>
2024-07-25 16:05:59 -0400

    questionable graph regarding source/modulation not fitting

--- code/minispect/raspberry_pi_firmware/utility/Camera_util.py
+++ code/minispect/raspberry_pi_firmware/utility/Camera_util.py
@@ -1,7 +1,65 @@
-from picamera2.encoders import H264Encoder
-from picamera2 import Picamera2, Preview
+#from picamera2.encoders import H264Encoder
+#from picamera2 import Picamera2, Preview
 import time
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
+CAM_FPS = 206.65
+
+def parse_video(path_to_video: str) -> np.array:
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    video_capture = cv2.VideoCapture(path_to_video)
+    video_capture.set(cv2.CAP_PROP_FOURCC, fourcc)
+
+    frames = []
+    while(True):
+        ret, frame = video_capture.read()
+
+        if(not ret): break 
+
+        frames.append(frame)
+
+    video_capture.release()
+
+    return np.array(frames, dtype=np.uint8)
+
+def analyze_temporal_sensitivity(video_frames: np.array):
+    # Convert video sequence to grayscale
+    grayscale_video: np.array = np.array([cv2.cvtColor(video_frames[i], cv2.COLOR_BGR2GRAY) for i in range(video_frames.shape[0])], dtype=np.uint8)
+
+    # Find average intensity of every frame in the video
+    average_frame_intensities: np.array = np.mean(grayscale_video, axis=(1,2))
+
+    """Express both source and observed values as power spectrum"""
+    frequency: int = 2  # frequency of the sinusoid in Hz
+    amplitude: int = 50   # amplitude of the sinusoid
+    phase: int = 0       # phase shift in radians
+    sampling_rate:int = 1000  # samples per second
+    duration: float = video_frames.shape[0] / CAM_FPS  # duration of the signal in seconds
+
+    # Generate mock source time values and sinusoidal wave 
+    t_source: np.array = np.linspace(0, duration, int(sampling_rate*duration), endpoint=False)
+    y_source: np.array = amplitude * np.sin(2 * np.pi * frequency * t_source + phase)   
+
+    # Generate x values in time for the measured points    
+    t_measured: np.array = np.linspace(0,duration,video_frames.shape[0],endpoint=False)
+    
+    """"""
+
+    # Plot the mock source data 
+    plt.plot(t_source, y_source, label='Source Modulation')
+
+    # Plot the observed data 
+    plt.plot(t_measured, average_frame_intensities, label='Avg Intensity')
+
+    plt.title('Camera Chip Temporal Sensitivity')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Amplitude')
+    plt.legend()
+    plt.show()
+
+"""
 def record_video(cam: Picamera2, encoder: H264Encoder, output_path: str):
     # Begin Recording with a preview
     cam.start_preview(Preview.QTGL)
@@ -44,18 +102,21 @@ def initialize_camera() -> Picamera2:
     
     return cam
 
-
+"""
 
 def main():
     # Initialize camera with our desired
     # settings
-    cam: Picamera2 = initialize_camera()
+    #cam: Picamera2 = initialize_camera()
     
     # Prepare encoder and output filename
-    encoder: H264Encoder = H264Encoder(bitrate=10000000)
-    output_file: str = 'test.h264'
+    #encoder: H264Encoder = H264Encoder(bitrate=10000000)
+    output_file: str = './test.h264'
     
-    record_video(cam, encoder, output_file)
+    #record_video(cam, encoder, output_file)
+
+    frames = parse_video(output_file)
+    analyze_temporal_sensitivity(frames)
 
 if(__name__ == '__main__'):
     main()
