801f5d630c90271598027b5a2ebf35c3e9cdb12b
Zachary Kelly <zachary.kelly@pennmedicine.upenn.edu>
2024-07-25 14:10:24 -0400

    works for 4, 5 NDF not 6

--- code/minispect/raspberry_pi_firmware/utility/Camera_util.py
+++ code/minispect/raspberry_pi_firmware/utility/Camera_util.py
@@ -21,31 +21,28 @@ def record_video(cam: Picamera2, encoder: H264Encoder, output_path: str):
     
 
 
-def initialize_camera():
+def initialize_camera() -> Picamera2:
     # Set up camera, turn off automatic
     # white balance, white point control,
     # turn on exposure control and
     # gain control
     
     # Initialize camera 
-    picam2: Picamera2 = Picamera2()
+    cam: Picamera2 = Picamera2()
     
     # Select the mode to put the sensor in
     # (ie, mode for high fps/low res, more pixel HDR etc)
-    sensor_mode: dict = picam2.sensor_modes[4]					# Other params ignored, so force the mode by setting these
-    #config = picam2.create_video_configuration(queue=False, sensor={'output_size': sensor_mode['size'], 'bit_depth': sensor_mode['bit_depth']})
+    sensor_mode: dict = cam.sensor_modes[4]					# Other params ignored, so force the mode by setting these
+    config = cam.create_video_configuration(sensor={'output_size': sensor_mode['size'], 'bit_depth': sensor_mode['bit_depth']})
     
     # Set the mode 
-    picam2.configure(picam2.create_video_configuration())
+    cam.configure(cam.create_video_configuration())
     
     # Set runtime camera information, such as auto-gain
     # auto exposure, white point balance, etc
-    picam2.set_controls({'AeEnable':True, 'AwbEnable':False}) # Note, AeEnable changes both AEC and AGC 
+    cam.set_controls({'AeEnable':True, 'AwbEnable':False}) # Note, AeEnable changes both AEC and AGC 
     
-    
-    picam2.title_fields = ['ExposureTime','AnalogueGain']
-    
-    return picam2
+    return cam
 
 
 
