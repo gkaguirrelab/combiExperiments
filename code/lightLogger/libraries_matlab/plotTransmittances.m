4f8f99c1ce2b182fbd66cd72ba869be7d41e2eb2
Zachary Kelly <zjkelly19@gmail.com>
2024-09-13 13:13:01 -0400

    updated plotTransmittances from Geoff

--- code/lightLogger/libraries_matlab/plotTransmittances.m
+++ code/lightLogger/libraries_matlab/plotTransmittances.m
@@ -3,9 +3,9 @@ calLocalData = getpref('combiLEDToolbox','CalDataFolder');
 figure
 hold on
 
-smoothParam = 1e-1;
+smoothParam = 0.02;
 
-for ii = 0:3
+for ii = 0:4
     calFileName = sprintf('CombiLED_shortLLG_sphere_ND%d_maxSpectrum.mat',ii);
     load(fullfile(calLocalData,calFileName),'cals');
     calSet{ii+1} = cals{end};
@@ -13,13 +13,16 @@ for ii = 0:3
         calSet{ii+1}.rawData.gammaCurveMeanMeasurements ./ ...
         calSet{1}.rawData.gammaCurveMeanMeasurements;
     wls = SToWls(calSet{1}.rawData.S);
-    plot(wls,log10(transmittance(ii+1,:)));
+    y = log10(transmittance(ii+1,:));
+    goodIdx = isfinite(y);
+    plot(wls(goodIdx),y(goodIdx));
     % label
-    yPos = log10(transmittance(ii+1,1))*1.1;
+    yPos = y(1)-0.3;
     text(min(wls),yPos,sprintf('ND%d',ii));
     % add a spline fit
-    yFit = csaps(wls,log10(transmittance(ii+1,:)),smoothParam,wls);
-    plot(wls,yFit,'-k')
+    yFit = csaps(wls(goodIdx),y(goodIdx),smoothParam,wls(goodIdx));
+    plot(wls(goodIdx),yFit,'-k')
+    ylim([yPos,0])
 end
 
 ylabel('log transmittance');
