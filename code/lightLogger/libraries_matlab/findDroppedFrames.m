e555f72db42183999cc04a772050f6ab7d1d62c5
Zachary Kelly <zjkelly19@gmail.com>
2024-10-23 14:43:33 -0400

    change default value to be consistent between finding and plotting

--- code/lightLogger/libraries_matlab/findDroppedFrames.m
+++ code/lightLogger/libraries_matlab/findDroppedFrames.m
@@ -2,7 +2,7 @@ function flagged_indices = findDroppedFrames(signal, threshold)
     
 arguments 
     signal {mustBeVector}; 
-    threshold (1,1) {mustBeNumeric} = prctile(signal, 99);
+    threshold (1,1) {mustBeNumeric} = prctile(signal, 98);
 end
 
 
