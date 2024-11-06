3c5e8b0e975312e7836d30547313674eb727e6f9
gkaguirre <gkaguirre@me.com>
2024-11-06 12:55:35 -0500

    Plot edits

--- code/experiments/plotDMTFVarianceAndBias.m
+++ code/experiments/plotDMTFVarianceAndBias.m
@@ -51,13 +51,13 @@ end
 
 
 % obtain a linear bias and variance across frequency
-[~,edges] = histcounts(log10(refFreq),10);
+[~,edges] = histcounts(log10(refFreq),7);
 Y = discretize(log10(refFreq),edges);
 nBins = length(edges)-1;
 
 for bb = 1:nBins
     logVariance(bb) = std(log10(testFreq(Y==bb))).^2;
-    linearBias(bb) = mean(testFreq(Y==bb)-refFreq(Y==bb));
+    linearBias(bb) = median(testFreq(Y==bb)-refFreq(Y==bb));
     binLabels{bb} = sprintf('%2.1f - %2.1f',round(10^edges(bb),1),round(10.^edges(bb+1),1));
 end
 
@@ -67,15 +67,15 @@ figuresize(600, 400,'pt');
 tiledlayout(2,3,"TileSpacing","tight","Padding","tight");
 nexttile([2 2]);
 
-loglog([1.5 15],[1.5 15],'--k','LineWidth',1.5)
+loglog([.25 50],[.25 50],'--k','LineWidth',1.5)
 hold on
 scatter(refFreq,testFreq,'k','filled','o','MarkerEdgeColor','none','MarkerFaceAlpha',0.25);
-xlim([1.5 15])
-ylim([1.5 15])
+xlim([.5 60])
+ylim([.5 60])
 axis square
 a=gca();
-a.XTick = [2:10];
-a.YTick = [2:10];
+a.XTick = [0.5 1 2 4 8 16 32];
+a.YTick = [0.5 1 2 4 8 16 32];
 xlabel('Reference Frequency [Hz]')
 ylabel('Match Frequency [Hz]')
 box off
@@ -100,7 +100,7 @@ hold on
 scatter(1:nBins,linearBias,'filled','o','MarkerEdgeColor','none','MarkerFaceColor',[0.5 0.5 0.5])
 axis square
 xlabel('Frequency bin [Hz]')
-ylabel('Bias [Hz]')
+ylabel('Median bias [Hz]')
 xlim([0.5,nBins+0.5]);
 a = gca();
 a.XTick = [1:nBins];
