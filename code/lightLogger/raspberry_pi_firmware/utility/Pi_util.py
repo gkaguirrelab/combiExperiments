ee8140838128b8eaa476d37d3feaabe63b18c73b
Zachary Kelly <zjkelly19@gmail.com>
2024-11-19 09:15:15 -0500

    update MATLAB chunk parsing to use structs

--- code/lightLogger/raspberry_pi_firmware/utility/Pi_util.py
+++ code/lightLogger/raspberry_pi_firmware/utility/Pi_util.py
@@ -19,21 +19,50 @@ def parse_chunk_paths(experiment_path: str) -> list:
     # Iterate over the burst names and build the filepaths
     # all of a given burst's readings
     for burst_idx, burst_name in enumerate(burst_names):
-        # Construct filepaths to all of the readings for a given bursts 
-        # by iterating over the files in the directory and finding 
-        # those that contain the burst name. Sort so it is always in
-        # the same order
-        burst_readings: list = sorted((os.path.join(experiment_path, file)
-                                       for file in experiment_files
-                                       if burst_name in file))
+        # Initialize an empty dictionary for all sensors
+        chunk_dict: dict = {name: [] 
+                           for name in ('MS', 'Pupil', 'World', 'Sunglasses')}
+        
+        # Find all of the files of this burst
+        burst_files: list = (os.path.join(experiment_path, file)
+                             for file in experiment_files
+                             if burst_name in file)
+        
+        # Next, we will assign the files to their respective sensors
+        for file in burst_files:
+            # Append world sensor files to that category 
+            if('world' in os.path.basename(file).lower()):
+                chunk_dict['World'].append(file)
+            
+            # Append MS sensor files to that category
+            elif('ms_readings' in os.path.basename(file).lower()):
+                chunk_dict['MS'].append(file)
+            
+            # Append pupil sensor files to that category
+            elif('pupil' in os.path.basename(file).lower()):
+                chunk_dict['Pupil'].append(file)
+            
+            # THE .TXT HERE IS A KLUDGE. I FORGOT TO PUT SUNGLASSES
+            # IN SOME FILENAMES FOR TESTING. IN THE FUTURE 
+            # THE SUNGLASSES WILL BE ALL THAT IS NEEDED
+            # Append sunglasses sensor files to that category
+            elif('sunglasses' in os.path.basename(file).lower() 
+                 or '.txt' in os.path.basename(file).lower()):
+                 chunk_dict['Sunglasses'].append(file)
+
+        # Go over all of the keys and values and sort the values 
+        # so keys with multiple values always have their 
+        # values show up in the same order 
+        for key, val in chunk_dict.items():
+            chunk_dict[key].sort()
 
         # Append this chunks' readings to the growing list
-        sorted_chunks.append(burst_readings)
+        sorted_chunks.append(chunk_dict)
 
     return sorted_chunks
 
 def main():
-    pass 
+    pass  
 
 if(__name__ == '__main__'):
     pass
