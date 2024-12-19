9f30405e42e2d506690caa19d911f90b762f3eb4
Zachary Kelly <Zachary.Kelly@pennmedicine.upenn.edu>
2024-12-19 15:13:20 +0000

    double buffer and write in parallel

--- code/lightLogger/raspberry_pi_firmware/rpi_cpp.cpp
+++ code/lightLogger/raspberry_pi_firmware/rpi_cpp.cpp
@@ -107,6 +107,92 @@ int write_process(fs::path& output_dir, std::vector<std::vector<uint8_t>>& buffe
     return 0; 
 }
 
+int write_process_parallel(const fs::path* output_dir, 
+                           const uint32_t duration, 
+                           const uint8_t buffer_size_s,
+                           std::vector<std::vector<uint8_t>>* buffers_one, 
+                           std::vector<std::vector<uint8_t>>* buffers_two) 
+    {
+    // Let the user know the write process started successfully 
+    std::cout << "Write | Initialized" << '\n';
+    
+    // Capture the start time of this process
+    auto start_time = std::chrono::steady_clock::now();
+    auto last_write_time = std::chrono::steady_clock::now();
+
+    // Define a counter to keep track of which write we are on 
+    uint32_t write_num = 1;
+
+    // Define a buffer pointer to use to switch between the buffers when writing.
+    // Initialize it as buffers one
+    std::vector<std::vector<uint8_t>>* buffer = buffers_one; 
+
+    // Write at regular intervals until the end of the recording 
+    std::cout << "Write | Beginning waiting for writes..." << '\n';
+    while(true) {
+        // Capture the elapsed time since the start
+        auto current_time = std::chrono::steady_clock::now();
+        auto elapsed_time = current_time - start_time;
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsed_time).count();
+        auto time_since_last_write = std::chrono::duration_cast<std::chrono::seconds>(current_time - last_write_time).count();
+
+        // End recording if we have reached the desired duration. 
+        if ((uint32_t) elapsed_seconds >= duration) {
+            break;
+        }
+
+        // If we have filled at least one buffer and it is a few seconds into the next buffer, 
+        // write out the previous buffer and clear it for the next swap 
+        if(time_since_last_write >= (buffer_size_s + 2)) {
+            // Begin timing how long writing this chunk took
+            auto start_write_time = std::chrono::steady_clock::now();
+            std::cout << "Write | Writing buffer: " << write_num << '\n';  
+
+            // Generate the filename for this chunk's output
+            fs::path filename = "chunk_" + std::to_string(write_num) + ".bin";
+
+            // Open a file in the output directory for writing 
+            std::ofstream out_file(*output_dir / filename, std::ios::binary);
+
+            // Ensure the file was opened correctly 
+            if(!out_file.is_open()) {
+                std::cerr << "ERROR: Failed to open outfile: " << *output_dir / filename << '\n';
+                exit(1);
+            }
+
+            // Retrieve the buffer to be written 
+            if(write_num % 2 == 0) {
+                buffer = buffers_two;
+            }
+            else {
+                buffer = buffers_one; 
+            }
+
+            { // Must force archive to go out of scope, ensuring all contents are flushed
+                cereal::BinaryOutputArchive out_archive(out_file);
+                out_archive(*buffer);
+            } // Source: https://uscilab.github.io/cereal/quickstart.html under Serialize your data
+            
+            // Close the output file
+            out_file.close();
+            
+            // Output how long writing this chunk took
+            auto elapsed_time_writing = std::chrono::steady_clock::now() - start_write_time;
+            std::cout << "Write | Writing buffer: " << write_num << " Took(ms): " << std::chrono::duration<float_t, std::milli>(elapsed_time_writing).count() << '\n';  
+
+
+            // Update the last time we wrote to the current time 
+            last_write_time = current_time;
+
+            // Incremement the write num
+            write_num++;
+        }
+
+    }
+
+    return 0; 
+}
+
 
 /*
 Continous recorder for the MS. 
@@ -114,7 +200,11 @@ Continous recorder for the MS.
 @Ret: 0 on success, errors and quits otherwise. 
 @Mod: N/A
 */
-int minispect_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
+int minispect_recorder(const uint32_t duration, 
+                       std::vector<uint8_t>* buffer_one, 
+                       std::vector<uint8_t>* buffer_two,
+                       const uint16_t buffer_size_frames) 
+    {
     // Create a boost io_service object to manage the IO objects
     // and initialize serial port variable
     boost::asio::io_service io;
@@ -165,9 +255,15 @@ int minispect_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
         if(ms.is_open()) { ms.close();}
         exit(1);
     }
+
+    // Set the initial buffer pointer to buffer 1 and track which one 
+    // we are on 
+    std::vector<uint8_t>* buffer = buffer_one;
+    uint8_t current_buffer = 1;
+
     std::cout << "MS | Initialized." << '\n';
 
-    // Begin recording
+    // Begin recording for the given duration
     std::cout << "MS | Beginning recording..." << '\n';
     auto start_time = std::chrono::steady_clock::now(); // Capture the start time 
     while(true) {
@@ -179,6 +275,22 @@ int minispect_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
         if ((uint32_t) elapsed_seconds >= duration) {
             break;
         }
+        
+        // Swap buffers if we filled up this buffer
+        if(frame_num > 0 && frame_num % buffer_size_frames == 0) {
+            // If we are currently using buffer 2, swap to 1
+            if(current_buffer % 2 == 0) {
+                buffer = buffer_one;
+            }
+            // If we are currently using buffer 1, swap to buffer 2
+            else {
+                buffer = buffer_two;
+            }
+
+            // Update the current buffer state
+            current_buffer = (current_buffer % 2) + 1;
+        }
+
 
         // Read a byte from the serial stream
         boost::asio::read(ms, boost::asio::buffer(byte_read));
@@ -226,12 +338,17 @@ Continous recorder for the World Camera.
 @Ret: 0 on success
 @Mod: N/A
 */
-int world_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
+int world_recorder(const uint32_t duration, 
+                   std::vector<uint8_t>* buffer_one, 
+                   std::vector<uint8_t>* buffer_two,
+                   const uint16_t buffer_size_frames) 
+    {
     // Initialize libcamera
     std::cout << "World | Initializating..." << '\n'; 
     //libcamera::CameraManager cameraManager;
 
-    // Initialize a counter for how many frames we are going to capture 
+    // Initialize a counter for how many frames we are going to capture, 
+    // and the size of our buffers 
     size_t frame_num = 0; 
 
     // Initialize a variable we will use to hold the mean of certain frames when we do AGC
@@ -240,10 +357,14 @@ int world_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
     // Initialize variables for the initial gain and exposure of the camera 
     float_t current_gain = 1;
     float_t current_exposure = 100; 
+
+    // Set the initial buffer pointer to buffer 1
+    std::vector<uint8_t>* buffer = buffer_one;
+    uint8_t current_buffer = 1;
     
     std::cout << "World | Initialized." << '\n';
 
-    // Begin recording
+    // Begin recording for the given duration
     std::cout << "World | Beginning recording..." << '\n';
     auto start_time = std::chrono::steady_clock::now(); // Capture the start time
     auto last_gain_change = start_time;
@@ -258,11 +379,26 @@ int world_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
             break;
         }
 
+        // Swap buffers if this one is full
+        if(frame_num > 0 && frame_num % buffer_size_frames == 0) {
+            // If we are currently using buffer 2, swap to 1
+            if(current_buffer % 2 == 0) {
+                buffer = buffer_one;
+            }
+            // If we are currently using buffer 1, swap to buffer 2
+            else {
+                buffer = buffer_two;
+            }
+
+            // Update the current buffer state
+            current_buffer = (current_buffer % 2) + 1;
+        }
+
         // Capture the desired frame
         int frame = 100;
 
         // Save the desired frame into the buffer (TODO: This is a kludge method just for now. ideally we will not be overwriting)
-        (*buffer)[frame_num % duration] = frame; 
+        (*buffer)[frame_num % buffer_size_frames] = frame; 
 
 
         // Adjust the AGC every 250MS
@@ -298,23 +434,30 @@ int world_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
     return 0;
 }
 
-
 /*
 Continous recorder for the Pupil Camera.
 @Param: duration: uint32_t - Duration of the recording in seconds.
 @Ret: 0 on success
 @Mod: N/A
 */
-int pupil_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
+int pupil_recorder(const uint32_t duration, 
+                   std::vector<uint8_t>* buffer_one, 
+                   std::vector<uint8_t>* buffer_two,
+                   const uint16_t buffer_size_frames) {
     // Initialize libUVC 
     std::cout << "Pupil | Initializating..." << '\n'; 
 
     // Initialize a counter for how many frames we are going to capture 
+    // and how big our buffer is
     size_t frame_num = 0; 
     
     std::cout << "Pupil | Initialized." << '\n';
 
-    // Begin recording
+    // Set the initial buffer pointer to buffer 1
+    std::vector<uint8_t>* buffer = buffer_one;
+    uint8_t current_buffer = 1;
+
+    // Begin recording for the given duration
     std::cout << "Pupil | Beginning recording..." << '\n';
     auto start_time = std::chrono::steady_clock::now(); // Capture the start time
     while(true) {
@@ -328,11 +471,26 @@ int pupil_recorder(uint32_t duration, std::vector<uint8_t>* buffer) {
             break;
         }
 
+        // Swap buffers if this one is full
+        if(frame_num > 0 && frame_num % buffer_size_frames == 0) {
+            // If we are currently using buffer 2, swap to 1
+            if(current_buffer % 2 == 0) {
+                buffer = buffer_one;
+            }
+            // If we are currently using buffer 1, swap to buffer 2
+            else {
+                buffer = buffer_two;
+            }
+
+            // Update the current buffer state
+            current_buffer = (current_buffer % 2) + 1;
+        }
+
         // Capture the desired frame
         int frame = 100;
 
         // Save the desired frame into the buffer (TODO: This is a kludge method just for now. ideally we will not be overwriting)
-        (*buffer)[frame_num % duration] = frame; 
+        (*buffer)[frame_num % buffer_size_frames] = frame; 
 
         // Increment the number of captured frames
         frame_num++;
@@ -361,10 +519,12 @@ int main(int argc, char **argv) {
     // Initialize controller flags as entirely false. This is because we will denote which controllers 
     // to use based on arguments passed. 
     constexpr std::array<char, 4> controller_names = {'M', 'W', 'P', 'S'};  // this can be constexpr because values will never change 
-    std::vector<std::function<int(int32_t, std::vector<uint8_t>*)>> controller_functions = {minispect_recorder, world_recorder, pupil_recorder}; // this CANNOT be constexpr because function stubs are dynamic
+    std::vector<std::function<int(int32_t, std::vector<uint8_t>*, std::vector<uint8_t>*, uint16_t)>> controller_functions = {minispect_recorder, world_recorder, pupil_recorder}; // this CANNOT be constexpr because function stubs are dynamic
     std::array<bool, 4> controller_flags = {false, false, false, false}; // this CANNOT be constexpr because values will change 
-    constexpr std::array<uint64_t, 4> data_size_multiplers = {148, 200*60*80, 120*400*400, 1}; // this can be constexpr because values will never change
-    
+    constexpr std::array<uint8_t, 4> sensor_FPS = {1, 200, 120, 1};
+    constexpr std::array<uint64_t, 4> data_size_multiplers = {sensor_FPS[0]*148, sensor_FPS[1]*60*80, sensor_FPS[2]*400*400, sensor_FPS[3]*1}; // this can be constexpr because values will never change
+    constexpr uint8_t sensor_buffer_size = 10; // Initialize a variable for the size of each sensors' buffer in seconds. This will be regularly written out and cleared
+
     // Parse the commandline arguments.
     if(parse_args(argc, (const char**) argv, output_dir, controller_flags, duration)) {
         std::cerr << "ERROR: Could not properly parse args." << '\n'; 
@@ -401,6 +561,7 @@ int main(int argc, char **argv) {
 
     std::cout << "Output Directory: " << output_dir << '\n';
     std::cout << "Duration: " << duration << " seconds" << '\n';
+    std::cout << "Buffer size: " << sensor_buffer_size << " seconds" << '\n';
     std::cout << "Num Active Controllers: " << num_active_sensors << '\n';
     std::cout << "Controllers to use: " << '\n';
     for(size_t i = 0; i < controller_names.size(); i++) {
@@ -408,24 +569,31 @@ int main(int argc, char **argv) {
     }
 
     // Once we know the duration and the sensors we are using, we are going to dynamically 
-    // allocate a buffer of duration seconds per sensor of 8bit values 
-    std::vector<std::vector<uint8_t>> buffers(controller_names.size()); // First, allocate an outer vector for all of the potential sensors
+    // allocate two buffers of duration seconds per sensor of 8bit values. This is because 
+    // we are going to spawn between them
     
+    // First, allocate outer vectors for all of the potential sensors
+    std::vector<std::vector<uint8_t>> buffers_one(controller_names.size());
+    std::vector<std::vector<uint8_t>> buffers_two(controller_names.size());
+
     // Iterate over the inner buffers and reserve enough memory + fill in dummy values for all of the readings.
     // Only do this for the sensors we are actually using
     for(const auto& controller_idx: used_controller_indices) { 
         // Mutiply the duration times the data size. 
         // For instance, the MS reads 148 bytes per second. 
         // The Pupil cam reads at 120x400x400
-        buffers[controller_idx].resize((duration + 1) * data_size_multiplers[controller_idx]);  // Allocate duration + 1 in case things read a little faster than normal
+
+        // Allocate time + 1 in case things read a little faster than normal
+        buffers_one[controller_idx].resize(sensor_buffer_size * data_size_multiplers[controller_idx]); 
+        buffers_two[controller_idx].resize(sensor_buffer_size * data_size_multiplers[controller_idx]); 
     }
 
     // Output information about how the buffer allocation process went
     std::cout << "----BUFFER ALLOCATIONS SUCCESSFUL---" << '\n';
-    std::cout << "Num buffers: " << buffers.size() << '\n';
-    std::cout << "Buffer capacities: " << '\n';
-    for(size_t i = 0; i < buffers.size(); i++) {
-        std::cout << '\t' << controller_names[i] << ": " << buffers[i].size() << '\n';
+    std::cout << "Num sensor buffers: " << buffers_one.size() << '\n';
+    std::cout << "Sensor buffer capacities(bytes): " << '\n';
+    for(size_t i = 0; i < buffers_one.size(); i++) {
+        std::cout << '\t' << controller_names[i] << ": " << buffers_one[i].size() << '\n';
     }
 
     // Begin parallel recording and enter performance critical section. All print statements below 
@@ -436,9 +604,17 @@ int main(int argc, char **argv) {
     // We will spawn only threads for those controllers that we are going to use. 
     // Spawn them, with the duration of recording as an argument
     std::cout << "----SPAWNING THREADS---" << '\n';
-    for (size_t i = 0; i < used_controller_indices.size(); i++) {
-        threads.emplace_back(std::thread(controller_functions[used_controller_indices[i]], duration, &buffers[i]));
+    for (const auto& used_controller_idx: used_controller_indices) {
+        threads.emplace_back(std::thread(controller_functions[used_controller_idx], 
+                                         duration,
+                                         &buffers_one[used_controller_idx], &buffers_two[used_controller_idx],
+                                         duration*sensor_FPS[used_controller_idx]));
     }
+
+    // We will also spawn the parallel write process, to monitor output from these threads
+    threads.emplace_back(std::thread(write_process_parallel, &output_dir, duration, 
+                                                             sensor_buffer_size,
+                                                             &buffers_one, &buffers_two));
 
     // Join threads to ensure they complete before the program ends
     for (auto& t : threads) {
@@ -448,14 +624,7 @@ int main(int argc, char **argv) {
     // Signal to the user that the threads has successfully closed their operation
     std::cout << "----THREADS CLOSED SUCCESSFULLY---" << '\n'; 
 
-    // Sequential write out just for testing 
-    auto start_write_time = std::chrono::steady_clock::now();
-    write_process(output_dir, buffers);
-    auto elapsed_time = std::chrono::steady_clock::now() - start_write_time;
-
-   std::cout << "----WRITING COMPLETE---" << '\n'; 
-   std::cout << "Write time(ms): " << std::chrono::duration<float_t, std::milli>(elapsed_time).count() << '\n';
-
+    // NEED TO ADD ONE FINAL WRITE TO CLEAR BUFFERS WHEN DURATION ENDS
 
     return 0; 
 }
