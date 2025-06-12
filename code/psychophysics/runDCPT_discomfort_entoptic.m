b87b4231c6baec3e3105e2bed3c87ff94cc3df6d
SamanthaAMontoya <155659827+SamanthaAMontoya@users.noreply.github.com>
2025-06-12 16:54:23 -0400

    Started new code to display all DCPT reference stimuli for discomfort and entoptic judgements

---
+++ code/psychophysics/runDCPT_discomfort_entoptic.m
@@ -0,0 +1,303 @@
+function runDCPT_discomfort_entoptic(subjectID,NDlabel,discomfortFlag,varargin)
+% Show modulations and collect discomfort/entoptic ratings
+%
+% Syntax:
+%   runDCPT_discomfort_entoptic(subjectID,NDlabel,discomfortFlag)
+%
+% Description:
+%   This function organizes the collection of data using the
+%   PsychDichopticFlickerDiscrim psychometric object. This is a measure of
+%   the ability of an observer to discriminate a change in the appearance
+%   of a flickering stimulus. On each of many trials the observer is shown
+%   a "reference" flicker on both sides of a dichoptic apparatus. After
+%   they have indicated readiness, the flicker is stopped and, after a
+%   brief ISI, a different, "test" flicker is presented on one side
+%   (selected randomly) while the reference flicker is re-presented on the
+%   other side. The obserer is asked to report which side has changed. A
+%   staircase or QUEST+ procedure is used to vary the test flicker to
+%   identify the frequency difference required to produce threshold
+%   discrimination performance.
+%
+%   This routine connects to the CombiLED stimulus devices, creates and
+%   subsequently loads the psychometric objects, and presents trials. Trial
+%   are organized into blocks. A given block presents each of several
+%   reference frequencies crossed with one or more reference contrasts.
+%   Blocks alternate between different photoreceptor directions.
+%
+% Inputs:
+%   subjectID             - Char vector or string. The ID of the subject.
+%   NDlabel               - Char vector or string. The ND filter used to
+%                           adjust the light level of the stimulus
+%   discomfortFlag        - Bool for running discomfort ratings (true) or
+%                           entoptic percept ratings (false)
+%
+% Optional key/value pairs:
+%  'modDirections'        - Cell array of char vectors. The names of the
+%                           modulation directions to study.
+%  'refFreqHz'            - Vector. The reference frequencies to study.
+%                           Typically, this is a log spaced set.
+%  'targetPhotoContrast'  - 2xd vector, where d is the number of modulation
+%                           directions to be studied. columns mod dir, rows
+%                           are contrast level. Provides the low and high
+%                           contrast levels to be studied. The default
+%                           values are approximately 10x and 30x detection
+%                           thresholds from pilot temporal contrast
+%                           sensitivity functions detection measures. We
+%                           found thresholds of approximately 0.005
+%                           contrast for L-M and 0.01 contrast for LF at
+%                           lower temporal frequencies. This seemed too
+%                           high fo L-M, so we used 5x and 15x instead.
+%
+% Outputs:
+%   none
+%
+% Examples:
+%{
+    subjectID = 'HERO_rsb';
+    NDlabel = '0x5';
+    discomfortFlag = 1;
+    runDCPT_discrim(subjectID,NDlabel, discomfortFlag);
+%}
+
+% Parse the parameters
+p = inputParser; p.KeepUnmatched = false;
+p.addParameter('modDirections',{'LminusM_wide','LightFlux'},@iscell);
+p.addParameter('refFreqHz',[3.0000    4.8206    7.7460   12.4467   20.0000],@isnumeric);
+p.addParameter('targetPhotoContrast',[0.025, 0.10; 0.075, 0.30],@isnumeric);
+p.addParameter('combiLEDLabels',{'C','D'},@iscell);
+p.addParameter('combiLEDIDs',{"A10L31XJ","A10L31XZ"},@iscell);
+p.addParameter('combiClockAdjust',[1.0006,0.9992],@isnumeric);
+p.addParameter('nBlocks',2, @isnumeric);
+p.addParameter('dropBoxBaseDir',getpref('combiExperiments','dropboxBaseDir'),@ischar);
+p.addParameter('dropBoxSubDir','FLIC_data',@ischar);
+p.addParameter('projectName','combiLED',@ischar);
+p.addParameter('verboseCombiLED',false,@islogical);
+p.addParameter('verbosePsychObj',true,@islogical);
+p.addParameter('simulateMode',false,@islogical);
+p.parse(varargin{:})
+
+%  Pull out some variablse from the p.Results structure
+modDirections = p.Results.modDirections;
+refFreqHz = p.Results.refFreqHz;
+targetPhotoContrast = p.Results.targetPhotoContrast;
+combiLEDLabels = p.Results.combiLEDLabels;
+combiLEDIDs = p.Results.combiLEDIDs;
+nBlocks = p.Results.nBlocks;
+useStaircase = p.Results.useStaircase;
+verboseCombiLED = p.Results.verboseCombiLED;
+verbosePsychObj = p.Results.verbosePsychObj;
+simulateMode = p.Results.simulateMode;
+combiClockAdjust = p.Results.combiClockAdjust;
+
+% Set our experimentName
+experimentName = 'DCPT';
+
+% Set the labels discomfort and entoptic
+if discomfortFlag
+    discomfortStr = {'discomfort'};
+else
+    discomfortStr = {'entoptic'};
+end
+
+% Define some basic trial type quantities
+nFreqs = length(refFreqHz);
+nConstrasts = size(targetPhotoContrast,1);
+nModDir = size(modDirections,1);
+nTrials = nFreqs*nConstrasts*nModDir;
+
+% Define the modulation and data directories
+subjectDir = fullfile(...
+    p.Results.dropBoxBaseDir,...
+    p.Results.dropBoxSubDir,...
+    p.Results.projectName,...
+    subjectID);
+
+% Load modResult files and extract the calibrations. We need this to
+% obtain a gamma table to pass to the combiLEDs, and this property of the
+% device does not change with modulation direction
+for side = 1:nSides
+    modResultFile{side} = ...
+        fullfile(subjectDir,[modDirections{1} '_ND' NDlabel],['modResult_' combiLEDLabels{side} '.mat']);
+    load(modResultFile{side},'modResult');
+    cal{side} = modResult.meta.cal;
+end
+
+% Set up the CombiLED
+if simulateMode
+    CombiLEDObjArr = {[],[]};
+else
+    % Open the CombiLED
+    for side = 1:nSides
+        CombiLEDObjArr{side} = CombiLEDcontrol('verbose',verboseCombiLED);
+    end
+
+    % Check the identifierString and swap objects if needed
+    if CombiLEDObjArr{1}.identifierString ~= combiLEDIDs{1} % wrong identifier
+        % Swap the objects
+        tempObj = CombiLEDObjArr{1};
+        CombiLEDObjArr{1} = CombiLEDObjArr{2};
+        CombiLEDObjArr{2} = tempObj;
+        clear tempObj
+    end
+
+    % Test that the CombiLED objects now have the correct identifier
+    % strings, and update the gamma table
+    for side = 1:nSides
+        assert(CombiLEDObjArr{side}.identifierString == combiLEDIDs{side});
+        CombiLEDObjArr{side}.setGamma(cal{side}.processedData.gammaTable);
+        CombiLEDObjArr{side}.setClockAdjustFactor(combiClockAdjust(side));
+    end
+end
+
+
+% Provide instructions
+if discomfortFlag 
+
+    fprintf('**********************************\n');
+    fprintf('On each of many trials you will be presented with flickering\n');
+    fprintf('lights in both eyes. When the lights stop flickering,\n');
+    fprintf('Your job is to rate your discomfort \n');
+    fprintf('from watching the lights on a scale from 1 to 10.');
+    fprintf('There are a total of %d trials.\n',nTrials);
+    fprintf('**********************************\n\n');
+
+else
+
+    fprintf('**********************************\n');
+    fprintf('On each of many trials you will be presented with flickering\n');
+    fprintf('lights in both eyes. You might see geometric or tree-like patterns\n');
+    fprintf('Your job is to report whether you see no spatial patterns (0), \n');
+    fprintf('geometric patterns (1), or tree-like patterns (2) after each trial.');
+    fprintf('There are a total of %d trials.\n',nTrials);
+    fprintf('**********************************\n\n');
+
+end
+
+% Prepare to loop over blocks
+for bb=1:nBlocks
+
+    % Switch back and forth between the modulation directions
+    directionIdx = mod(bb,2)+1;
+    
+
+    % Load the mod results for this direction for the two combiLEDs
+    for side = 1:nSides
+        modResultFile{side} = ...
+            fullfile(subjectDir,[modDirections{directionIdx} '_ND' NDlabel '_shifted'],['modResult_' combiLEDLabels{side} '.mat']);
+        load(modResultFile{side},'modResult');
+        modResultArr{side} = modResult;
+        clear modResult
+    end
+
+    % Create a directory for the subject
+    dataDir = fullfile(subjectDir,[modDirections{directionIdx} '_ND' NDlabel, '_shifted'],experimentName);
+    if ~isfolder(dataDir)
+        mkdir(dataDir)
+    end
+
+    % Assemble the psychObj array, looping over the high and low range of
+    % the discrimination function AND the reference frequencies AND the
+    % contrast
+    psychObjArray = cell(nFreqs,nConstrasts);
+    for freqIdx = 1:nFreqs
+        for contrastIdx = 1:nConstrasts
+
+            % Define the filestem for this psychometric object
+            dataDir = fullfile(subjectDir,[modDirections{directionIdx} '_ND' NDlabel, '_shifted'],experimentName);
+            psychFileStem = [subjectID '_' experimentName '_' discomfortStr];
+
+            % Create or load the psychometric object
+            psychObjFilename = fullfile(dataDir,[psychFileStem '.mat']);
+
+            % Store in the psychObjArray
+            psychObjArray{freqIdx, contrastIdx} = psychObj;
+
+            % Clear the psychObj
+            clear psychObj
+        end
+    end
+
+    % Initialize the display for one of the psychObj elements. This routine
+    % assumes that all of the psychObj elements that will be called during
+    % the block use the same modulation, modulation background, temporal
+    % profile (i.e., sinusoid), and trial duration.
+    psychObjArray{1,1,1}.initializeDisplay;
+
+    % Start the block
+    if ~simulateMode
+        fprintf('Press enter to start block %d...',bb);
+        input('');
+    end
+   
+    % TO DO make a set order we present to all participants.
+    % Create a random ordering of the three stimulus crossings (high and
+    % low range, frequencies, contrast levels)
+    nReps = nTrialsPerBlock / (nRanges*nFreqs*nConstrasts);
+    triplets = zeros(nRanges*nFreqs*nConstrasts,3);
+    [a,b,c] = ndgrid(1:nRanges,1:nFreqs,1:nConstrasts);
+    triplets(:,1) = a(:); triplets(:,2) = b(:); triplets(:,3) = c(:);
+    triplets = repmat(triplets,nReps,1);
+    permutedTriplets = triplets(randperm(nTrialsPerBlock),:);
+
+    % Store the block start time
+    for freqIdx = 1:nFreqs
+        for contrastIdx = 1:nConstrasts
+            blockStartTime = datetime();
+            psychObjArray{freqIdx, contrastIdx}.blockStartTimes(psychObjArray{freqIdx, contrastIdx}.blockIdx) = blockStartTime;
+        end
+    end
+    %% STOPPED HERE
+    % Present nTrials
+    for ii = 1:nTrialsPerBlock
+        psychObjArray{...
+            permutedTriplets(ii,1),...
+            permutedTriplets(ii,2),...
+            permutedTriplets(ii,3)}.presentTrial
+    end
+
+    % Report completion of this block
+    fprintf('done.\n');
+
+    % Store the psychObjArray entries
+    for rangeIdx = 1:nRanges
+        for freqIdx = 1:nFreqs
+            for contrastIdx = 1:nConstrasts
+                % Grab the next psychObj
+                psychObj = psychObjArray{rangeIdx, freqIdx, contrastIdx};
+                % empty the CombiLEDObj handles and save the psychObj
+                psychObj.CombiLEDObjArr = {};
+                % Save the psychObj
+                save(psychObj.filename,'psychObj');
+            end
+        end
+    end
+    % Make a sound for the end of the block
+    BlockDone.fs = 8000;              % Sampling frequency (Hz)
+    duration = 0.25;         % Duration (seconds)
+    freq = 600;            % Tone frequency (Hz)
+    freq2 = 1200;
+    t = 0:1/BlockDone.fs:duration;    % Time vector
+    BlockDone.y = sin(2*pi*freq*t);   % Generate sine wave
+    BlockDone.y2 = sin(2*pi*freq2*t);
+    
+    if bb<nBlocks
+        sound(BlockDone.y, BlockDone.fs);           % Play the sound
+        pause (0.26);
+        sound(BlockDone.y2, BlockDone.fs);           % Play the sound
+    end
+end % block loop
+
+% Clean up
+if ~simulateMode
+    for side = 1:nSides
+        CombiLEDObjArr{side}.goDark;
+        CombiLEDObjArr{side}.serialClose;
+    end
+end
+clear CombiLEDObjArr
+
+% Tell participant the task is done
+ExperimentDone = load('handel');
+sound(ExperimentDone.y,ExperimentDone.Fs)
+
+end % function
