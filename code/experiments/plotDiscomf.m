8af8f7119a769cd1750db9625153720a2d484e3f
kristinakitsul <kristinakitsul1@gmail.com>
2025-06-23 16:53:06 -0400

    Added plotting to the discomfort analysis code.

--- code/experiments/plotDiscomf.m
+++ code/experiments/plotDiscomf.m
@@ -2,8 +2,12 @@ function [outputArg1,outputArg2] = plotDiscomf(subjectID, modDirections, NDLabel
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 % which directions to analyze
-%modDirections = {'LminusM_wide' 'LightFlux'};
-%NDLabel = {'0x5'};
+%{
+subjectID = 'HERO_kik';
+modDirections = {'LminusM_wide' 'LightFlux'};
+NDLabel = {'0x5'};
+plotDiscomf(subjectID, modDirections, NDLabel);
+%}
 
 modDirectionsLabels = {'LminusM', 'LightFlux'}; 
 
@@ -27,8 +31,38 @@ for directionIdx = 1:length(modDirections)
     contrast(directionIdx, :) = psychObj.contrastOrder;
     refFreq(directionIdx, :) = psychObj.refFreqOrder;
 end
-%% Plot the data
+%% Plot the data 
+hold on;
+
+colors = {'r', 'k'};
+markers = {'o', 's'};
+
+for directionIdx = 1:length(modDirections)
+    dataDir = fullfile(subjectDir,[modDirections{directionIdx} '_ND' NDLabel{1} '_shifted'],experimentName);
+    y = ratings(directionIdx, :);
+    x = refFreq(directionIdx, :);
+    c = contrast(directionIdx, :);
+
+    if contains (modDirections{directionIdx}, 'LightFlux')
+        color = colors{1};
+        marker = markers{1};
+    else 
+        color = color{2};
+        marker = marker{2};
+    end
+plot(x, y, {color marker '-'}, 'LineWidth', 1.5, 'DisplayName', modDirections{directionIdx});
+end
+
+xlabel('Freqeuncy');
+ylabel('Discomford Rating');
+titel('Discomfort Ratings by Frequency and Contrast');
+legend('Location', 'north' );
+hold off; 
+
+   
+       
+
+
 
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
-end
