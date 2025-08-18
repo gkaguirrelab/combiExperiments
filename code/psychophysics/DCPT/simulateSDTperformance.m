e59fdbfecdb59633388ac631a105d1053e875261
gkaguirre <gkaguirre@me.com>
2025-08-18 14:37:58 -0400

    Create the mod array

--- code/psychophysics/DCPT/simulateSDTperformance.m
+++ code/psychophysics/DCPT/simulateSDTperformance.m
@@ -1,8 +1,17 @@
+    cal = loadCalByName('CombiLED-C_irFilter-C_cassette-C_ND0_classicEyePiece-C.mat','DCPT');
+    observerAgeInYears = 53;
+    pupilDiameterMm = 3;
+    photoreceptors = photoreceptorDictionaryHuman('observerAgeInYears',observerAgeInYears,'pupilDiameterMm',pupilDiameterMm);
+    whichDirection = 'LightFlux';
+    modResult = designModulation(whichDirection,photoreceptors,cal);
+modResultArr{1} = modResult;
+modResultArr{2} = modResult;
 
-simulatePsiParams = [0.05,1,1];
-nTrials = 50;
+
+simulatePsiParams = [0.05,0.75,1.5];
+nTrials = 40;
 nullTrialRate = []; correctRate= []; paramSet=[];
-for kk = 1:50
+for kk = 1:10
     clear obj
     obj = PsychDichopticFlickerSDT([], modResultArr, [], 5,...
         'simulateMode',true,'simulatePsiParams',simulatePsiParams);
