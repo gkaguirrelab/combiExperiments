a7d303eaf3fba1e1caac3aef81466f92096800a0
npcottaris <cottaris@upenn.edu>
2025-11-04 10:33:55 -0500

    Update to handle a vector of stim dB differences

--- code/psychophysics/DCPT/modifiedSameDiffModel.m
+++ code/psychophysics/DCPT/modifiedSameDiffModel.m
@@ -25,9 +25,9 @@ function pDifferent = modifiedSameDiffModel( stimDiffDb, p )
 % Examples:
 %{
     % Define some params
-    p = [1.4, 2.5, 0.5, 1]
-    stimDiffDb = -5:0.5:5;
-    pDifferent = modifiedSameDiffModel( stimDiffDb, p );
+    p = [1.4, 2.5, 0.5, 1];
+    stimDiffDb = -5:0.5:5;    
+    pDifferent = modifiedSameDiffModel(stimDiffDb, p );
     plot(stimDiffDb, pDifferent,'*-r');
 %}
 
@@ -39,35 +39,30 @@ x_limit = p(4);
 
 % First calculate the "c" value, which is the criterion that the observer
 % uses to determine if same or different given the internal measurement.
-if abs(stimDiffDb) <= x_limit
-    c = crit_baseline - m * (x_limit - abs(stimDiffDb));
-else
-    c = crit_baseline;
-end
+c = crit_baseline - m * max(0, (x_limit - abs(stimDiffDb)));
+% If abs(stimDiffDb) <= x_limit, we will have c = crit_baseline;
 
-% Parameters
-mu_R = 0;     % mean of reference
-mu_T = stimDiffDb;     % mean of test
-
-% Function for the joint PDF f(mR, mT)
-f = @(mR, mT) normpdf(mR, mu_R, sigma) .* normpdf(mT, mu_T, sigma);
-
-% The integral limits are defined by the criterion: mR - c <= mT <= mR + c
+% Integral limits
 mR_min = -inf;
 mR_max = inf;
 
-% Lower limit for mT: g(mR) = mR - c
-g = @(mR) mR - c;
+% Loop for integral evaluation
+for ii = 1:numel(stimDiffDb)
+    mu_R = 0;
+    mu_T = stimDiffDb(ii);
 
-% Upper limit for mT: h(mR) = mR + c
-h = @(mR) mR + c;
+    % Define joint PDF
+    f = @(mR, mT) normpdf(mR, mu_R, sigma) .* normpdf(mT, mu_T, sigma);
 
-try
-    P_same_integral2 = integral2(f, mR_min, mR_max, g, h);
-catch
-    P_same_integral2 = NaN;
+    % Integration bounds for mT given mR
+    g = @(mR) mR - c(ii);
+    h = @(mR) mR + c(ii);
+
+    % Compute probability of "same"
+    P_same = integral2(f, mR_min, mR_max, g, h);
+
+    % Probability of "different"
+    pDifferent(ii) = 1 - P_same;
 end
-
-pDifferent = 1 - P_same_integral2;
 
 end
