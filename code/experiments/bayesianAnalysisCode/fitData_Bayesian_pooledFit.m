a29e222b041913d307376daba4c1b34a6554378c
rubybouh <ruby.bouhassira@pennmedicine.upenn.edu>
2026-01-23 14:54:14 -0500

    file naming adjustments

--- code/experiments/bayesianAnalysisCode/fitData_Bayesian_pooledFit.m
+++ code/experiments/bayesianAnalysisCode/fitData_Bayesian_pooledFit.m
@@ -13,7 +13,8 @@ experimentName = 'DCPT_SDT';
 %         'FLIC_1034','FLIC_1035','FLIC_1036','FLIC_1038', 'FLIC_1041', 'FLIC_1044'};
 % subjectID = {'FLIC_1016','FLIC_1029','FLIC_1030','FLIC_1031','FLIC_1032', ...
 %         'FLIC_1034','FLIC_1035','FLIC_1036','FLIC_1038', 'FLIC_1041', 'FLIC_1044'};
-subjectID = {'FLIC_1016'};
+subjectID = {'FLIC_1016','FLIC_1029','FLIC_1030','FLIC_1031','FLIC_1032', ...
+    'FLIC_1034','FLIC_1035','FLIC_1036','FLIC_1038', 'FLIC_1041', 'FLIC_1044'};
 modDirection = 'LightFlux';
 NDLabel = {'3x0', '0x5'};   % {'3x0', '0x5'}
 stimParamLabels = {'low', 'hi'}; % {'low', 'hi'}
@@ -28,6 +29,7 @@ nSubj = length(subjectID);
 
 %% FITTING CODE %%
 
+% Want to plot it on top of the pooled data
 % Pooled sigma fit (across subjects, reference freqs, and sides)
 
 priorSame = 0.5;
