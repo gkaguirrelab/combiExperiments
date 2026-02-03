209e3cc4525fdeb2329698f24fa21981d6fda86b
gkaguirre <gkaguirre@me.com>
2026-02-03 14:32:57 -0500

    Listing subjects in order of completed full data set

--- code/psychophysics/PUFF/analysis/combineSubjectData.m
+++ code/psychophysics/PUFF/analysis/combineSubjectData.m
@@ -2,8 +2,8 @@ close all
 clear
 
 % Define the list of subjects
-subjects = {'BLNK_1001','BLNK_1003','BLNK_1006','BLNK_1007',...
-    'BLNK_1009','BLNK_1010','BLNK_1011'};
+subjects = {'BLNK_1007','BLNK_1006','BLNK_1001',...
+    'BLNK_1009','BLNK_1011'};
 
 % Define temporal properties of the recording
 options.fps = 180;
