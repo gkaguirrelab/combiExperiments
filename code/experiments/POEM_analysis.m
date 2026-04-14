b8684edb690ad3db85ab4bc8bf8c5cc26617df18
npcottaris <ruby.bouhassira@pennmedicine.upenn.edu>
2026-04-14 15:08:10 -0400

    Changed location of POEM survey file

--- code/experiments/POEM_analysis.m
+++ code/experiments/POEM_analysis.m
@@ -1,9 +1,9 @@
 % To efficiently process and analyze responses to the POEM survey for our
 % DCPT_discrim experiment
 
-fileName = 'POEM_v3.3 -- CHYPS_December 10, 2025_09.28.csv';
+fileName = 'POEM_v3.3 -- CHYPS_March 6, 2026_09.22.xlsx';
 
-spreadsheet = ['/Users/melanopsin/Aguirre-Brainard Lab Dropbox/Ruby Bouhassira/FLIC_subject/POEM raw data files Summer 2025/' fileName];
+spreadsheet = ['/Users/melanopsin/Aguirre-Brainard Lab Dropbox/Ruby Bouhassira/FLIC_analysis/dichopticFlicker/surveyData/' fileName];
 T = poemAnalysis_preProcess_v3(spreadsheet);
 
 poemAnalysis_classify_v3(T)
