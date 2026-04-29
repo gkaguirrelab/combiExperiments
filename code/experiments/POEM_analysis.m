dd33810a491c404abb66eb894e58e1df82993ef9
rubybouh <ruby.bouhassira@pennmedicine.upenn.edu>
2026-04-29 17:43:05 -0400

    Reporting more CHYPS details and making a new survey data table

--- code/experiments/POEM_analysis.m
+++ code/experiments/POEM_analysis.m
@@ -1,9 +1,9 @@
 % To efficiently process and analyze responses to the POEM survey for our
 % DCPT_discrim experiment
 
-fileName = 'POEM_v3.3 -- CHYPS_March 6, 2026_09.22.xlsx';
+fileName = 'POEM_subjID_only.xlsx';
 
-spreadsheet = ['/Users/melanopsin/Aguirre-Brainard Lab Dropbox/Ruby Bouhassira/FLIC_analysis/dichopticFlicker/surveyData/' fileName];
+spreadsheet = ['/Users/rubybouh/Aguirre-Brainard Lab Dropbox/Ruby Bouhassira/FLIC_analysis/dichopticFlicker/surveyData/' fileName];
 T = poemAnalysis_preProcess_v3(spreadsheet);
 
 poemAnalysis_classify_v3(T)
--- code/experiments/analyzeDemographic/subjSummaryData.m
+++ code/experiments/analyzeDemographic/subjSummaryData.m
@@ -1,46 +1,155 @@
-% This code creates a demographic table from the FLIC subject summary Excel spreadsheet.
+% This code generates tables of FLIC demographic and clinical characteristics.
 
-% Information stratified by group includes age, number of women, race
-% composition of the sample, and number of hispanic individuals
-% Add BCVA and color blindness information, not sure how to summarize
+% Define directories
+dropBoxBaseDir = getpref('combiExperiments','dropboxBaseDir');
+dropBoxSubDir = 'FLIC_analysis';
+dropBoxSummaryDir = 'FLIC_subject'; % for subjSummary spreadsheet only
+projectName = 'dichopticFlicker';
+experimentName = 'surveyData';
 
-subjSummaryFile = '/Users/rubybouh/Aguirre-Brainard Lab Dropbox/Ruby Bouhassira/FLIC_subject/FLIC_SubjectSummary.xlsx';
-
+% Load subj summary excel sheet
+subjSummaryDataDir = fullfile(dropBoxBaseDir, dropBoxSummaryDir);
+subjSummaryFile = fullfile(subjSummaryDataDir,'/FLIC_SubjectSummary.xlsx'); 
 % Detect the default options for this file
 opts = detectImportOptions(subjSummaryFile);
 % Set where the Variable Names (titles) and Data start
 % Headers are in Row 1 and data starts in Row 2:
 opts.VariableNamesRange = 'A1'; 
 opts.DataRange = 'A2';
-
 % Read the Excel file into a table
-T = readtable(subjSummaryFile, opts);
-
+subjSummaryT = readtable(subjSummaryFile, opts);
 % Rename variables to clean names
-T = renamevars(T, {'SubjectID__'},{'SubjectID'}); 
-
+subjSummaryT = renamevars(subjSummaryT, {'SubjectID__'},{'SubjectID'}); 
 % Last subject is FLIC_0051, want the table to end here
-idx_end = find(strcmp(T.SubjectID, 'FLIC_0051'));
-T = T(1:idx_end, :);
-
+idx_end = find(strcmp(subjSummaryT.SubjectID, 'FLIC_0051'));
+subjSummaryT = subjSummaryT(1:idx_end, :);
 % Remove subjects who did not participate (have a star next to ID)
-hasStar = contains(T.SubjectID, '*');
-T = T(~hasStar, :);
-
+hasStar = contains(subjSummaryT.SubjectID, '*');
+subjSummaryT = subjSummaryT(~hasStar, :);
 % Convert text columns to string arrays
-T.MigraineOrControl_ = string(T.MigraineOrControl_);
-T.SexAssignedAtBirth = string(T.SexAssignedAtBirth);
-T.NIHRace = string(T.NIHRace);
-T.NIHEthnicity = string(T.NIHEthnicity);
+subjSummaryT.MigraineOrControl_ = string(subjSummaryT.MigraineOrControl_);
+subjSummaryT.SexAssignedAtBirth = string(subjSummaryT.SexAssignedAtBirth);
+subjSummaryT.NIHRace = string(subjSummaryT.NIHRace);
+subjSummaryT.NIHEthnicity = string(subjSummaryT.NIHEthnicity);
+% Sort order to be migrainers -> control subjects in increasing order
+subjSummaryT.idNum = str2double(extractAfter(subjSummaryT.SubjectID,"FLIC_"));
+subjSummaryT = sortrows(subjSummaryT, {'MigraineOrControl_','idNum'});
+subjSummaryT.idNum = []; % remove temp variable
+
+% Load POEM data excel sheet
+poemDataDir = fullfile(dropBoxBaseDir, dropBoxSubDir, projectName, experimentName);
+poemFile = fullfile(poemDataDir,'/POEM_subjID_only.xlsx'); 
+% Process this spreadsheet with code from POEM_analysis repo
+preProcessT = poemAnalysis_preProcess_v3(poemFile);
+poemT = poemAnalysis_classify_v3(preProcessT);
+% Sort order to be migrainers -> control subjects in increasing order
+poemT.idNum = str2double(extractAfter(poemT{:,1},"FLIC_"));
+poemT = sortrows(poemT, {'Migraine','idNum'});
+poemT.idNum = []; % remove temp variable
+
+%% 
+% This subject makes a table with: 
+% Info stratified by group includes no of women, age, headache days/1 mo,
+% CHYPS score, MIDAS score, medication use last month
+
+% Extracting no of women and age from subjSummary doc
+% Extracting headache days and MIDAS from POEM
+% Have not added in medication use yet
+
+groups = {'Control','Migraine with aura'};
 
 % Compute age summary
-groups = {'Control','Migraine'};
+age_summary = strings(length(groups),1);
+for i = 1:length(groups)
+    idx = subjSummaryT.MigraineOrControl_ == groups{i};
+    mean_age = mean(subjSummaryT.Age(idx));
+    std_age = std(subjSummaryT.Age(idx));
+    age_summary(i) = sprintf('%.1f ± %.1f', mean_age, std_age);
+end
+
+% Compute number of women
+sex_summary = strings(length(groups),1);
+for i = 1:length(groups)
+    idx = subjSummaryT.MigraineOrControl_ == groups{i};
+    n_women = sum(subjSummaryT.SexAssignedAtBirth(idx) == "Female");
+    n_total = sum(idx);
+    sex_summary(i) = sprintf('%d/%d', n_women, n_total);
+end
+
+% Clinical variables to summarize (mean ± SD)
+varNames = { ...
+    'MonthlyMigraineFrequency', ...
+    'MIDAS_score', ...
+    'CHYPS_total_score', ...
+    'CHYPS_Brightness', ...
+    'CHYPS_Pattern', ...
+    'CHYPS_Strobing', ...
+    'CHYPS_IntenseVisEnviro'};
+
+var_summary = strings(length(groups), length(varNames));
+
+for v = 1:length(varNames)
+    thisVar = varNames{v};
+
+    for i = 1:length(groups)
+        idx = subjSummaryT.MigraineOrControl_ == groups{i};
+
+        data = poemT.(thisVar);
+        mean_val = mean(data(idx), 'omitnan');
+        std_val  = std(data(idx), 'omitnan');
+
+        var_summary(i,v) = sprintf('%.1f ± %.1f', mean_val, std_val);
+    end
+end
+
+% Combine into table
+summaryTable = table( ...
+    groups', ...
+    age_summary, ...
+    sex_summary, ...
+    var_summary(:,1), ...
+    var_summary(:,2), ...
+    var_summary(:,3), ...
+    var_summary(:,4), ...
+    var_summary(:,5), ...
+    var_summary(:,6), ...
+    var_summary(:,7), ...
+    'VariableNames', { ...
+        'Group', ...
+        'Age_mean_SD', ...
+        'Female_n_over_total', ...
+        'MonthlyMigraineFrequency', ...
+        'MIDAS_score', ...
+        'CHYPS_total', ...
+        'CHYPS_Brightness', ...
+        'CHYPS_Pattern', ...
+        'CHYPS_Strobing', ...
+        'CHYPS_IntenseVisEnviro'});
+
+disp(summaryTable)
+
+% poemT.MonthlyMigraineFrequency;
+% poemT.MIDAS_score;
+% poemT.CHYPS_total_score;
+% poemT.CHYPS_Brightness;
+% poemT.CHYPS_Pattern;
+% poemT.CHYPS_Strobing;
+% poemT.CHYPS_IntenseVisEnviro
+
+%%
+% This section makes a table with:
+% Information stratified by group includes age, number of women, race
+% composition of the sample, and number of hispanic individuals
+% Add BCVA and color blindness information, not sure how to summarize
+
+% Compute age summary
+groups = {'Control','Migraine with aura'};
 age_summary = strings(length(groups),1);
 
 for i = 1:length(groups)
-    idx = T.MigraineOrControl_ == groups{i};
-    mean_age = mean(T.Age(idx));
-    std_age = std(T.Age(idx));
+    idx = subjSummaryT.MigraineOrControl_ == groups{i};
+    mean_age = mean(subjSummaryT.Age(idx));
+    std_age = std(subjSummaryT.Age(idx));
     age_summary(i) = sprintf('%.1f ± %.1f', mean_age, std_age);
 end
 
@@ -48,8 +157,8 @@ end
 sex_summary = strings(length(groups),1);
 
 for i = 1:length(groups)
-    idx = T.MigraineOrControl_ == groups{i};
-    n_women = sum(T.SexAssignedAtBirth(idx) == "Female");
+    idx = subjSummaryT.MigraineOrControl_ == groups{i};
+    n_women = sum(subjSummaryT.SexAssignedAtBirth(idx) == "Female");
     n_total = sum(idx);
     sex_summary(i) = sprintf('%d/%d', n_women, n_total);
 end
@@ -65,11 +174,11 @@ race_categories = [   % NIH race categories
 race_composition = strings(length(groups),1);
 
 for i = 1:length(groups)
-    idx = T.MigraineOrControl_ == groups{i};
+    idx = subjSummaryT.MigraineOrControl_ == groups{i};
     n_total = sum(idx);
     parts = strings(1, length(race_categories));
     for j = 1:length(race_categories)
-        n_race = sum(contains(T.NIHRace(idx), race_categories(j)));
+        n_race = sum(contains(subjSummaryT.NIHRace(idx), race_categories(j)));
         pct = round(100 * n_race / n_total);
         if n_race > 0
             parts(j) = sprintf('%s %d (%d%%)', race_categories(j), n_race, pct);
@@ -84,8 +193,8 @@ end
 % Ethnicity summary
 ethnicity_summary = zeros(length(groups),1);  % just number of Hispanic
 for i = 1:length(groups)
-    idx = T.MigraineOrControl_ == groups{i};
-    ethnicity_summary(i) = sum(T.NIHEthnicity(idx) == "Hispanic or Latino");
+    idx = subjSummaryT.MigraineOrControl_ == groups{i};
+    ethnicity_summary(i) = sum(subjSummaryT.NIHEthnicity(idx) == "Hispanic or Latino");
 end
 
 % Combine into a table
@@ -98,15 +207,6 @@ demographics_table.NumberHispanic = ethnicity_summary;
 
 disp(demographics_table)
 
-
-%% Subject demographic and clinical characteristics
-
-% Info stratified by group includes no of women, age, headache days/1 mo,
-% CHYPS score, MIDAS score, medication use last month
-
-% Have number of women, age, and CHYPS score from subj summary doc
-
-% Need to extract headache days, MIDAS, medication use from POEM
 
 
 %% Code specifically to make a table of the ages of all FLIC participants 
@@ -136,10 +236,10 @@ opts.DataRange = 'A2';
 
 % Read the Excel files into tables
 TOld = readtable(oldFilename, optsOld);
-T = readtable(filename, opts);
+subjSummaryT = readtable(filename, opts);
 
 % Extract the Age column
-ages = [TOld.Age; T.Age];
+ages = [TOld.Age; subjSummaryT.Age];
 
 % Define bin edges
 edges = [0 2 6 13 18 26 46 65 76 inf];
